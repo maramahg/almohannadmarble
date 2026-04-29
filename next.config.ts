cda7ea4d20a7eb786b3d78c2e843c4d114a1d4de
maramahg <maramalghamdi441@gmail.com>
2026-04-29 18:20:28 +0300

    fix: allow images from S3 bucket in next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'almohannad.s3.eu-north-1.amazonaws.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
